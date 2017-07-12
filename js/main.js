50be6ded534e3b741c969b1587573a74c8922486
enqtran <enqtran@gmail.com>
2017-07-12 10:36:27 +0700

    up

--- js/main.js
+++ js/main.js
@@ -1,6 +1,40 @@
 const socket = io('http://localhost:3000');
 
+$('#div-chat').hide();
+
+let customConfig;
+
+// Call XirSys ICE servers
+$.ajax({
+  url: "https://service.xirsys.com/ice",
+  data: {
+    ident: "enqtran",
+    secret: "966b2eb0-66ac-11e7-aa8a-5af66f31fc42",
+    domain: "http://enqtrandev.website/webrtc",
+    application: "AppName",
+    room: "RoomName",
+    secure: 1
+  },
+  success: function (data, status) {
+    // data.d is where the iceServers object lives
+    customConfig = data.d;
+    console.log(customConfig);
+  },
+  async: false
+});
+  
+// PeerJS object
+// var peer = new Peer({
+//   key: 'Your PeerServer cloud API key',
+//   debug: 3,
+//   config: customConfig
+// });
+
 socket.on('DANH_SACH_ONLINE', data => {
+
+    $('#div-chat').show();
+    $('#div-dangky').hide();
+
     data.forEach(data => {
         const {username, id } = data;
         $('#ulUser').append(`<li id="${id}">${username}</li>`);
@@ -10,7 +44,18 @@ socket.on('DANH_SACH_ONLINE', data => {
         const {username, id } = user;
         $('#ulUser').append(`<li id="${id}">${username}</li>`);
     });
+
+    socket.on('AI_DO_NGAT_KET_NOI', peerId => {
+        $(`#${peerId}`).remove();
+    });
+
+
+
+
 });
+
+
+socket.on('DANG_KY_THAT_BAI', () => alert('Username da ton tai, hay chon username khac !'));
 
 
 
@@ -27,7 +72,16 @@ function playStream(idVideoTag, stream) {
 
 //openSteam().then( stream => playStream('localStream', stream));
 
-const peer = new Peer({ key: 'lwjd5qra8257b9' });
+// const peer = new Peer({ key: 'lwjd5qra8257b9' });
+const peer = new Peer({ 
+    key: 'peerjs', 
+    host: 'webrtc-enqtran.herokuapp.com', 
+    secure: true, 
+    port: 443, 
+    config: customConfig 
+});
+
+
 peer.on('open', id => { 
     $('#my-peer').append(id);
     $('#btnSignUp').click(() => {
@@ -70,6 +124,17 @@ peer.on('call', call => {
 
 
 
+$('#ulUser').on('click', 'li', function(){
+    const id = $(this).attr('id');
+    openSteam()
+        .then(stream => {
+            console.log(stream);
+
+            playStream('localStream', stream);
+            const call = peer.call(id, stream);
+            call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+        });
+});
 
 
 
--- server/index.js
+++ server/index.js
@@ -7,9 +7,24 @@ io.on('connection', socket => {
     console.log(socket.id);
 
     socket.on('NGUOI_DUNG_DANG_KY', user => {
+        const isExist = arrUsername.some(e => e.username === user.username);
+
+        socket.peerId = user.id;
+
+        if(isExist) return socket.emit('DANG_KY_THAT_BAI');
+
         arrUsername.push(user);
 
         socket.emit('DANH_SACH_ONLINE', arrUsername);
         socket.broadcast.emit('CO_NGUOI_DUNG_MOI', user);
     });
+
+    socket.on('disconnect', () => {
+        const index = arrUsername.findIndex(user => user.id === socket.peerId);
+        arrUsername.splice(index,1);
+
+        io.emit('AI_DO_NGAT_KET_NOI', socket.peerId);
+    });
+
+
 });
